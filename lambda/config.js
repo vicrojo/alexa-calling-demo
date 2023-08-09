1f355642f2ff54e6f68dfd2da265ff8954d72f2b
Victor Rojo <vicrojo@amazon.com>
2023-08-08 20:30:53 -0700

    Initial Version

---
+++ lambda/config.js
@@ -0,0 +1,10 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+const skillConfig = {
+  destinationPhoneNumber: '<ChimeSDKPhoneNumber>',
+  sourcePhoneNumber: '<YourAmazonNumber>',
+};
+
+module.exports = {
+  skillConfig,
+};
